9c73747541021750f8fcff50229c446d3a7a6154
sigwaldostepan <sigwaldos@gmail.com>
2025-02-09 11:18:41 +0700

    configurated env files

--- api/src/common/database/database.module.ts
+++ api/src/common/database/database.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { Customer } from 'src/features/customer/customer.entity';
 import { Product } from 'src/features/product/product.entity';
 import { Stock } from 'src/features/stock/stock.entity';
@@ -8,15 +9,21 @@ import { Transaction } from 'src/features/transaction/entities/transaction.entit
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'sales',
-      entities: [Product, Stock, Transaction, TransactionDetail, Customer],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => (
+        {
+          type: 'postgres',
+          host: configService.get<string>('DB_HOST'),
+          port: configService.get<number>('DB_PORT'),
+          username: configService.get<string>('DB_USERNAME'),
+          password: configService.get<string>('DB_PASSWORD'),
+          database: configService.get<string>('DB_NAME'),
+          entities: [Product, Stock, Transaction, TransactionDetail, Customer],
+          synchronize: (configService.get<string>('NODE_ENV') === 'development') ? true : false
+        }
+      )
     }),
   ],
 })
--- api/src/features/app/app.module.ts
+++ api/src/features/app/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config'
 import { ProductModule } from 'src/features/product/product.module';
 import { StockModule } from 'src/features/stock/stock.module';
 import { TransactionModule } from '../transaction/transaction.module';
@@ -9,6 +10,10 @@ import { DatabaseModule } from 'src/common/database/database.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.development.local', '.env']
+    }),
     DatabaseModule,
     ProductModule,
     StockModule,
