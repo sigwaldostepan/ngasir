a14fe5507312cc827bf452c93c8538756ff1e496
unknown <sigwaldos@gmail.com>
2025-02-11 17:30:19 +0700

    enabled cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,6 +5,9 @@ import { ValidationPipe } from './common/pipes/custom-validation.pipe';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://localhost:3000', process.env.ALLOWED_ORIGIN],
+  });
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(process.env.PORT ?? 4000);
